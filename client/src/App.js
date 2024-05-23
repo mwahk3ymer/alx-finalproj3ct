1b84c037c0b27122bc2093f00fe388f241c61171
Mwahk3ymer <audreymwakima99@gmail.com>
2024-05-23 16:10:45 +0300

    tailwind 23.05 16.00hrs

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <h1>Ecommerce app</h1>
+      <h1 className="text red 600">Ecommerce app</h1>
     </div>
   );
 }
